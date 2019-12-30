c44357d18de93479fd7f53076271987a49285345
Jorge Franco <mail@lipsoft.pt>
2019-12-30 15:13:01 -0100

    test coverage

--- test/common/index.js
+++ test/common/index.js
@@ -1,9 +1,51 @@
 'use strict'
+/* eslint-env mocha */
+const {
+  loadYamlFile,
+  loadTemplate,
+  render,
+  saveFile,
+  fileExists,
+  capitalize,
+  flatten
+} = require('../../lib/common')
 
-const { capitalize } = require('../../lib/common')
+const rimraf = require('rimraf')
+const path = require('path')
+const assert = require('assert')
+const chai = require('chai')
+const sinon = require('sinon')
 
-var assert = require('assert')
+sinon.assert.expose(chai.assert, { prefix: '' })
+rimraf.sync(path.normalize(path.join(__dirname, '../../test/build/common/dist')))
 
-describe('#indexOf()', function () {
-  it('should return Amazing ', () => assert.equal(capitalize('amazing'), 'Amazing'))
+describe('#common()', function () {
+  describe('#loadYamlFile()', function () {
+    it('should return js Object ', () => assert.deepStrictEqual(loadYamlFile('../../test/build/common/loadYamlFile.yaml'), require('../../test/build/common/loadYamlFile.json')))
+    it('should return js Object ', () => assert.deepStrictEqual(loadYamlFile('..\\..\\test\\build\\common\\loadYamlFile.yaml'), require('../../test/build/common/loadYamlFile.json')))
+  })
+
+  describe('#loadTemplate()', function () {
+    it('should return Template ', () => assert.deepStrictEqual(loadTemplate('../../test/build/common/template.mustache'), 'oapiToFastify is {{{word}}}'))
+    it('should return Template ', () => assert.deepStrictEqual(loadTemplate('..\\..\\test\\build\\common\\template.mustache'), 'oapiToFastify is {{{word}}}'))
+  })
+
+  describe('#render()', function () {
+    it('should return render template ', () => assert.deepStrictEqual(render(loadTemplate('../../test/build/common/template.mustache'), { word: 'Awesome' }), 'oapiToFastify is Awesome'))
+    it('should return render template ', () => assert.deepStrictEqual(render(loadTemplate('..\\..\\test\\build\\common\\template.mustache'), { word: 'Awesome' }), 'oapiToFastify is Awesome'))
+  })
+
+  describe('#saveFile()', function () {
+    it('should save file ', () => assert.strictEqual(saveFile('../../test/build/common/dist/test.txt', 'oapiToFastify is awesome'), undefined))
+    it('file should exist ', () => assert.strictEqual(fileExists('../../test/build/common/dist/test.txt'), true))
+  })
+
+  describe('#capitalize()', function () {
+    it('should return \'\' ', () => assert.strictEqual(capitalize(''), ''))
+    it('should return Amazing ', () => assert.strictEqual(capitalize('amazing'), 'Amazing'))
+  })
+
+  describe('#flatten()', function () {
+    it('should return [1,2,3] ', () => assert.deepStrictEqual(flatten([1, [2, [3]]]), [1, 2, 3]))
+  })
 })
---
+++ test/oapiToModules/index.js
@@ -0,0 +1,34 @@
+'use strict'
+/* eslint-env mocha */
+const oapiToModules = require('../../lib/oapiToModules')
+
+const assert = require('assert')
+const chai = require('chai')
+const sinon = require('sinon')
+
+const openApi = require('../../test/build/oapiToModules/openapi.json')
+
+sinon.assert.expose(chai.assert, { prefix: '' })
+
+describe('#oapiToModules()', function () {
+  describe('#oapiToModules()', function () {
+    const modules = oapiToModules(openApi)
+    it('should be equal', () => assert.deepStrictEqual(modules[0], [
+      {
+        content: {},
+        path: 'src/modules/Error/entity.ts',
+        template: 'entity'
+      },
+      {
+        content: {},
+        path: 'src/modules/Error/routes.ts',
+        template: 'routes'
+      },
+      {
+        content: {},
+        path: 'src/modules/Error/schema.ts',
+        template: 'schema'
+      }
+    ]))
+  })
+})
